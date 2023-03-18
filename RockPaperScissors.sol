4efd6d9009eb9efa07f548626dd2a2f59a2e9b9e
poncj <91033092+poncj@users.noreply.github.com>
2023-03-19 00:27:19 +0600

    Update RockPaperScissors.sol

--- RockPaperScissors.sol
+++ RockPaperScissors.sol
@@ -10,6 +10,7 @@ contract RockPaperScissors {
     // 1 bnb = 10^9 gwei
     // minimum is 1000000 gwei or 10^15 wei
 
+
     mapping(uint8 => string) private label;
     mapping(uint8 => uint8) private target;
 
@@ -25,15 +26,15 @@ contract RockPaperScissors {
         target[2] = 1;
     }
 
-    event GamePlayed(address player, bool isWinner, string details);
+    event GamePlayed(address player, bool isWinner, string details, uint8 _optionByContract);
 
     function playGame(uint8 _optionByPlayer) payable public returns(bool) {
-    
         require(_optionByPlayer <=2, "Choose: 0 - Rock, 1 - Paper, 2 - Scissors");
         require(msg.value >= minBet, "Minimum bet is 0.001 tBNB");
         require(address(this).balance >= msg.value*2, "Too big a bet. The contract will not be able to pay the winnings.");
-        
-        uint8 _optionByContract = uint8(block.timestamp % 3);
+
+        uint256 pseudoRandom = uint256(keccak256(abi.encodePacked(block.timestamp, block.number, msg.sender, uint8(173)))); // 173 - solt
+        uint8 _optionByContract = uint8(pseudoRandom % 3);
         string memory details = '';
         
         if (target[_optionByPlayer] == _optionByContract) {
@@ -44,15 +45,16 @@ contract RockPaperScissors {
             details = string.concat(details, label[_optionByContract]);
             details = string.concat("Player won! ", details);
 
-            emit GamePlayed(msg.sender, true, details);
+            emit GamePlayed(msg.sender, true, details, _optionByContract);
             return true;
 
         } else if (_optionByPlayer == _optionByContract) {
-        
+
             payable(msg.sender).transfer(msg.value);
             
             details = string.concat("Its a Draw! Both players chose ", label[_optionByPlayer]);
-            emit GamePlayed(msg.sender, false, details);
+
+            emit GamePlayed(msg.sender, false, details, _optionByContract);
             return false;
 
         } else {
@@ -61,7 +63,7 @@ contract RockPaperScissors {
             details = string.concat(details, label[_optionByPlayer]);
             details = string.concat("Contract won! ", details);
             
-            emit GamePlayed(msg.sender, false, details);
+            emit GamePlayed(msg.sender, false, details, _optionByContract);
             return false;
         }
     }

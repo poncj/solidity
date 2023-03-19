6feed40e39a8182af6107185d3b9c9d951bd662d
poncj <91033092+poncj@users.noreply.github.com>
2023-03-19 18:44:50 +0600

    Update AirdropTokenTeam26.sol

--- AirdropTokenTeam26.sol
+++ AirdropTokenTeam26.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-interface ITeamToken26 {
+interface ITokenTeam26 {
     function transfer(address _to, uint256 _amount) external;
     function transferFrom(address _from, address _to, uint256 _amount) external;
 }
@@ -10,11 +10,11 @@ contract AirdropTokenTeam26 {
 
     address public owner;
     
-    ITeamToken26 public token;
+    ITokenTeam26 public token;
 
     constructor(address _tokenAddress) {
         owner = msg.sender;
-        token = ITeamToken26(_tokenAddress);
+        token = ITokenTeam26(_tokenAddress);
     }
 
     function airdropTokensFromAirdropBalance(address[] memory _addressArray, uint256[] memory _amountArray) public {

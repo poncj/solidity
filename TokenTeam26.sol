bd24de1192f5f2d0ecfedd732885e01db9b318b8
poncj <91033092+poncj@users.noreply.github.com>
2023-03-20 23:26:16 +0600

    Update TokenTeam26.sol

--- TokenTeam26.sol
+++ TokenTeam26.sol
@@ -7,13 +7,15 @@ contract TokenTeam26 is ERC20 {
 
     address public owner;
 
-    uint256 public mintValue = 10e17;
+    uint256 public mintValue = 10e17; // 1 ether or 1 token
 
     constructor() ERC20("TokenTeam26", "T26") {
         owner = msg.sender;
         _mint(owner, 10000 * mintValue);
     }
-
+    
+    
+    // FOR TESTING PURPOSES
     function mintTenTokens() external {
         _mint(msg.sender, mintValue);
     }
